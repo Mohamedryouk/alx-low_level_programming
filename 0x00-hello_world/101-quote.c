0922d1e4066920b4ba74444dae834ef497322f98
Mohamedryouk <mohammedabdullah.ism@gmail.com>
2023-08-11 23:48:53 -0700

    101-quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 /**
- *
  *main- main func
  *Return: return 0 right
  */
 int main(void)
 {
-	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19");
-	return (0);
+	fwrite(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 59);
+	return (1);
 }
