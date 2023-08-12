68bd3fab7f183525f93e0fe2a5db23976ba46811
Mohamedryouk <mohammedabdullah.ism@gmail.com>
2023-08-11 23:02:57 -0700

    hi

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ *
+ *main- main func
+ *Return: return 0 right
+ */
+int main(void)
+{
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	return (0);
+}
