e3de63414af30d6ba5a3f25f5abcef8c534e246d
Mohamedryouk <mohammedabdullah.ism@gmail.com>
2023-08-14 12:30:04 -0700

    alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ *main- mainfunc
+ *Return: 0 = success
+ */
+int main(void)
+{
+	char alphabet= 'a';
+	while (alphabet <= 'z')
+	{
+		putchar(alphabet);
+		alphabet++;
+	}
+	putchar('\n');
+	return (0);
+}
