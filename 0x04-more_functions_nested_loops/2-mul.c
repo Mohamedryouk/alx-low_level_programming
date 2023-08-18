093ee6d31ff7586d5ac2cd1daa407352d0190c39
Mohamedryouk <mohammedabdullah.ism@gmail.com>
2023-08-18 08:58:05 -0700

    hi

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *main- main function
+ *Description: function multiply two num
+ *@a: the first integer
+ *@b: integer two
+ *Return: return 0 after the multiplication
+ */
+int mul/*mul*/(int a, int b)
+{
+	int mult;
+	a = 98, b = 1024;
+	mult = a * b;
+	return (0)
+}
+
