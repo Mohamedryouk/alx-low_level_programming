f5cb694167f814ebd9190d02d9c8781fe085466b
Mohamedryouk <mohammedabdullah.ism@gmail.com>
2023-08-21 05:12:34 -0700

    hi

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ *_strlen- returns the length
+ *@s: pointer from type int
+ *Return: returns count
+ */
+int _strlen(char *s)
+{
+	int count;
+
+	for (count = 0 ; *s != '\0' ; count++)
+	return (count);
+}
