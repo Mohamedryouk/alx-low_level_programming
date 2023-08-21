74f882962f5265203d4cbd02bc1251c0cb0a73aa
Mohamedryouk <mohammedabdullah.ism@gmail.com>
2023-08-21 05:45:28 -0700

    hi

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ *_puts- prints string followed by new line
+ *@str: the string
+ */
+void _puts(char *str)
+{
+	for (*str = 0; *str != '\0'; *str++)
+	{
+		_putchar(*str);
+	}
+	_putchar('\n');
+}
+
