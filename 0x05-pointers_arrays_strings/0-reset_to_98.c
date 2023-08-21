31f6c2d4df7a0b3d7776e491dbd2818ce1ff328c
Mohamedryouk <mohammedabdullah.ism@gmail.com>
2023-08-21 04:19:05 -0700

    hi

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ *void reset_to_98(int *n) - to reset int from 402 to 98
+ *@n: int with value 402
+ *Return: always 0
+ */
+void reset_to_98(int *n)
+{
+	int m  = 98;
+	n = &m;
+
+	_putchar('n');
+}
