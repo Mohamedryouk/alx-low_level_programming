a0754a7c1068b29bc388a86c6a0b9b42fa7dc70f
Mohamedryouk <mohammedabdullah.ism@gmail.com>
2023-08-23 00:41:08 -0700

    hi

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * \*_strcat - concatenate
+ *@dest: var
+ *@src: var
+ *Return: returns char
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i = 0;
+	int j = 0;
+
+	while (dest[i++] != '\0')
+	{
+		dest++;
+		i++;
+	}
+	while (src[j] != 0)
+	{
+		*dest = *src;
+		dest++;
+		src++;
+		j++;
+	}
+	_putchar('\n');
+}
+
