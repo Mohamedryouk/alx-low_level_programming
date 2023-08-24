cfd79fa84bc76f2cbf2983d9dafb58f23a014076
Mohamedryouk <mohammedabdullah.ism@gmail.com>
2023-08-23 23:41:53 -0700

    hi

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _strncat - concatenate function
+ *
+ *@dest: destination
+ *@src: source
+ *@n: var
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i = 0;
+	int j = 0;
+
+	while (dest[i++])
+		j++;
+	for (i = 0; src[i] && i < n; i++)
+		dest[j++] = src[i];
+	return (dest);
+}
+
