aad087a376a81faa7c9e23266e3f3181c990281a
Mohamedryouk <mohammedabdullah.ism@gmail.com>
2023-08-23 23:24:05 -0700

    hi

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,13 +9,7 @@
  */
 char *_strcat(char *dest, char *src)
 {
-	int i = 0;
-	int j = strlen(dest);
-
-	for (i = 0; src[i] != '\0'; i++)
-	{
-		dest[i + j] = src[i];
-	}
-	dest[i + j] = '\0';
+	strcat(dest, src);
+	return(dest);
 }
 
