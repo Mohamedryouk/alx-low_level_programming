a6d83b8fefc0b4bd500a2c63c065f2d01d54c75c
Mohamedryouk <mohammedabdullah.ism@gmail.com>
2023-08-24 00:22:09 -0700

    2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -10,15 +10,10 @@
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i = 0;
-	int j = 0;
 
-	while (src[i++])
-		j++;
-	for (i = 0; src[i]  && i < n; i++)
+	for (i = 0; src[i] != '\0'  && i < n; i++)
 	{
 		src[i] = dest[i];
 	}
-	for (i = j; i < n; i++)
-		dest[i] = '\0';
-	return (src)
+	return (src);
 }
