ff41461231c836d1ce454abaf2a0a96426119b37
Mohamedryouk <mohammedabdullah.ism@gmail.com>
2023-08-24 02:17:32 -0700

    3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -7,20 +7,15 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int flg = 0;
 	int i = 0;
 
 	while (s1[i] != '\0' && s2[i] != '\0')
 	{
 		if (s1[i] != s2[i])
 		{
-			flg = 1;
-			break;
+			return (s1[i] - s2[i]);
 		}
 		i++;
 	}
-	if (flg == 0)
-		return (0);
-	else
-		return (1);
+	return (0);
 }
