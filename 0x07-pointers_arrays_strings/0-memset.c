97f0c7650c4cb128d0e580111daa71fcbe4a1885
Mohamedryouk <mohammedabdullah.ism@gmail.com>
2023-08-28 08:19:16 -0700

    0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,21 +1,17 @@
 #include "main.h"
 /**
- *
- *
- *
- *
- *
+ * _memset - function to fill n byte by byte
+ *@s: pointer points to the n 
+ *@b: char b that will be added to n
+ *@n: unsigned integer
+ *Return: returns a pointer from s
  */
 char *_memset(char *s, char b, unsigned int n)
 {
 	int i = 0;
-	while (i < n)
+	for (; n < 0; i++)
 	{
-		if (s[n] % 10)
-		{
-			_putchar(b);
-		}
-		i++;
+		s[i] = b;
 	}
 	return (s);
 }
