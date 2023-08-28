002576f7a10784f68f4c46a01de426bf4c3d062c
Mohamedryouk <mohammedabdullah.ism@gmail.com>
2023-08-28 10:43:54 -0700

    3-strspn.c

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int j;
+
+	while (*s)
+	{
+		for (j = 0; accept[j]; j++)
+		{
+			if (*s == accept[j])
+				return (s);
+			s++;
+		}
+		return ('\0');
+	}
+}
