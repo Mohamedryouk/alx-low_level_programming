255fb7ad1b92144094ccf1bbb17a4af96a7e9dd3
Mohamedryouk <mohammedabdullah.ism@gmail.com>
2023-08-31 13:11:33 -0700

    sqrt

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *is_prime_number
+ *
+ *@n: func num
+ *
+ *Return: zero
+ */
+int is_prime_number(int n)
+{
+	if (i == 1)
+		return (1);
+	if (n % i == 0 && i > 0)
+		return (0);
+	return (actual_prime(n, i - 1));
+}
