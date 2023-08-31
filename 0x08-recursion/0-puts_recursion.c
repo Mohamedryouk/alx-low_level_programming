a07a0d521934a0da98b0aeb784a259a62d4970c9
Mohamedryouk <mohammedabdullah.ism@gmail.com>
2023-08-31 07:06:37 -0700

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,13 +1,19 @@
 #include "main.h"
 /**
+ *_puts_recursion - func
  *
+ *@s: pointer to char
  *
- *
- *
+ *Return: return void
  */
 void _puts_recursion(char *s)
 {
-	if (*s == '\0')
-		return;
-	_putchar(*s);
+	char i;
+	for(; *s < '\0'; *s++)
+	{
+		if (*s == '\0')
+			return;
+		_putchar(*s);
+	}
+	_putchar('\n');
 }
