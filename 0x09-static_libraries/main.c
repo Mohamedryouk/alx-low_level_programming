99a7fa40d495b314e5233dd4b1af475f826a04a3
Mohamedryouk <mohammedabdullah.ism@gmail.com>
2023-09-02 08:44:19 -0700

    hi

--- 0x09-static_libraries/main.c
+++ 0x09-static_libraries/main.c
@@ -1,12 +1,5 @@
-#include "main.h"
-/**
- *main - function main
- *
- *Return: return 0 when executed
- */
 int main(void)
 {
-	_puts("\"At the end of the day, my goal was to be the best hacker
-	\"\n\t- Kevin Mitnick");
+	_puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
 	return (0);
 }
