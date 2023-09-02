fb7fb935920285044035b59f455d6ae91a99507f
Mohamedryouk <mohammedabdullah.ism@gmail.com>
2023-09-02 10:40:47 -0700

    hi

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -4,9 +4,9 @@
  *@argc: int
  *@argv: list
  */
-void main(int argc, char *argv[])
+int main(int argc, char *argv[])
 {
 	(void)argc;
-	printf("\s\n", argv[0]);
+	printf("%s\n", argv[0]);
 	return (0);
 }
