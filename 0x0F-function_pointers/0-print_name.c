4e601cb7ada7d623554a64839974997b70299c4c
Mohamedryouk <mohammedabdullah.ism@gmail.com>
2023-09-13 10:12:14 -0700

    define

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -3,10 +3,10 @@
  *print_name- function name
  *
  *@name: name string
- *@(*f)(char *): pointer to function
+ *@f: pointer to function
  */
 void print_name(char *name, void (*f)(char *))
 {
 	if (name && f)
-		f(name);;
+		f(name);
 }
