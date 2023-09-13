c55297fb0d124da3b5928eaa1ec16f155482dc2d
Mohamedryouk <mohammedabdullah.ism@gmail.com>
2023-09-13 09:16:54 -0700

    Function to pointer

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ *print_name- function name
+ *
+ *@name: name string
+ *@(*f)(char *): pointer to function
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f() = print_name(name);
+}
