b42f13852710f83c145b3e646140312de84642e6
Mohamedryouk <mohammedabdullah.ism@gmail.com>
2023-09-16 01:14:16 -0700

    hi

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ *_strdup- prototpye function
+ *
+ *@str: pointer
+ *
+ *Return: return 0 after execution
+ *
+ */
+char *_strdup(char *str)
+{
+	int i = 0, size = 0;
+	char *new_str;
+
+	if (str == NULL)
+		return (NULL);
+	for (; str[size] != '\0'; size++)
+	;
+
+	new_str = malloc(size * sizeof(*str) + 1);
+
+	if (new_str == 0)
+		return (NULL);
+	else
+	{
+		for (; i < size; i++)
+			new_str[i] = str[i];
+	}
+	return (new_str);
+}
