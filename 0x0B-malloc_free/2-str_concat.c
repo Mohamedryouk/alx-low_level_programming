b575e494608e74dbcc4333bc53ce21c361b2ea51
Mohamedryouk <mohammedabdullah.ism@gmail.com>
2023-09-16 02:50:02 -0700

    hi

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -28,17 +28,19 @@ char *str_concat(char *s1, char *s2)
 		s1 = "\0";
 	if (s2 == NULL)
 		s2 = "\0";
+
 	size1 = _strlen(s1);
 	size2 = _strlen(s2);
 	new_str = malloc((size1 + size2) * sizeof(char) + 1);
 	if (new_str == 0)
 		return (0);
+
 	for (i = 0; i <= size1 + size2; i++)
 	{
 		if (i < size1)
 			new_str[i] = s1[i];
 		else
-			new_str[i] = s2[i + size2];
+			new_str[i] = s2[i - size2];
 	}
 	new_str[i] = '\0';
 	return (new_str);
