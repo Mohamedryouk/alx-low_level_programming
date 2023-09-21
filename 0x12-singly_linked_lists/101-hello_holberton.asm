abeb89693646ca4e6a23619a43e532676710894c
Mohamedryouk <mohammedabdullah.ism@gmail.com>
2023-09-21 10:42:54 -0700

    done

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,19 +1,9 @@
-section .data
-    hello db 'Hello, Holberton', 0xA
-    hello_len equ $-hello
-
-section .text
-    global _start
-
-_start:
-    ; write the string to stdout
-    mov eax, 4
-    mov ebx, 1
-    mov ecx, hello
-    mov edx, hello_len
-    int 0x80
-
-    ; exit the program
-    mov eax, 1
-    xor ebx, ebx
-    int 0x80
+global   main
+	  extern    printf
+main:
+	  mov   edi, format
+	  xor   eax, eax
+	  call  printf
+	  mov   eax, 0
+	  ret
+format: db `Hello, Holberton\n`,0
