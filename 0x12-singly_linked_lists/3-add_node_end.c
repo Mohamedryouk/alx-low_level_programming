13233160a410e9c9d9deb12a8332fa9b53c735b6
Mohamedryouk <mohammedabdullah.ism@gmail.com>
2023-09-21 08:41:39 -0700

    done

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,31 @@
+#include "lists.h"
+/**
+ *add_node_end- inserting a node at the end of linked lists
+ *@head: pointer to func
+ *@str: string of char pointer
+ *Return: return 0
+ */
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t  *ptr;
+	list_t *temp = *head;
+	unsigned int len;
+
+	while (str[len])
+		len++;
+	ptr = malloc(sizeof(list_t));
+	if (!ptr)
+		return (NULL);
+	ptr->str = strdup(str);
+	ptr->len = len;
+	ptr->next = NULL;
+	if (*head == NULL)
+	{
+		*head = ptr;
+		return (ptr);
+	}
+	while (temp->next)
+		temp = temp->next;
+	temp->next = ptr;
+	return (ptr);
+}
