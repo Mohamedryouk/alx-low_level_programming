6cc6cd62d4ac4f099794894dbdb018592061e5bf
Mohamedryouk <mohammedabdullah.ism@gmail.com>
2023-10-03 12:16:07 -0700

    done

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -2,7 +2,7 @@
 /**
  *create_array- function array
  *@size: look for the size of array of chars
- *@c: assginig the number of chars in thenarray to c
+ *@c: assginig the number of chars in then array to c
  *Return: return type to return the array
  */
 char *create_array(unsigned int size, char c)
