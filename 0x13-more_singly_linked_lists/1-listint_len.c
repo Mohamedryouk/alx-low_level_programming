ee2ce63183eb8737f670cd8d0d5bfe12efd29c97
Mohamedryouk <mohammedabdullah.ism@gmail.com>
2023-10-24 07:56:45 -0700

    done

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+/**
+ * listint_len- function to get the numbers of the elements in list
+ * @h: pointer to the next node
+ * Return: returns the number of the element in the list
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t num = 0;
+	while(h != NULL)
+	{
+		num++;
+		h = h->next;
+	}
+	return (num);
+}
