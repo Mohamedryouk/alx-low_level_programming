da2a9535df37aa691fe42466c8daceabf913d8f8
Mohamedryouk <mohammedabdullah.ism@gmail.com>
2023-10-24 12:22:06 -0700

    done

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -9,11 +9,11 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	unsigned int i;
 	listint_t *tmp = head;
-	
+
 	for (i = 0; i < index && tmp != NULL; i++)
 	{
 		tmp = tmp->next;
 	}
-	
+
 	return (tmp);
 }
