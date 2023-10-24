e551054ecb1d96fa3534dce75b5103b919b21983
Mohamedryouk <mohammedabdullah.ism@gmail.com>
2023-10-24 06:44:33 -0700

    done

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/*
+ * print_listint- prints the elements of the lists
+ * @h: list pointer to head
+ * Return: returns the number of the nodes
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		count++;
+	}
+	return (count);
+}
---
+++ 0x13-more_singly_linked_lists/lists.h
@@ -0,0 +1,18 @@
+#ifndef LISTS_H
+#define LISTS_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+/*
+ * Node- struct name
+ * @data: struct memeber
+ * @next: struct pointer
+ */
+struct list {
+	int n;
+	struct list* next;
+};
+typedef struct list listint_t;
+size_t print_listint(const listint_t *h);
+#endif /*lists.h*/
