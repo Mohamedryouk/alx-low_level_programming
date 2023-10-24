17710fc8773b2736529ed002423343c89143c018
Mohamedryouk <mohammedabdullah.ism@gmail.com>
2023-10-24 10:24:40 -0700

    done

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * free_listint2- frees one list
+ * @head: head of list
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *current;
+
+	while (*head != NULL)
+	{
+		current = *head;
+		*head = (*head)->next;
+		free(current);
+	}
+	*head = NULL;
+}
