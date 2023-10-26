500e65aeb4fe918af622215dfd0b6548be0e52d1
Mohamedryouk <mohammedabdullah.ism@gmail.com>
2023-10-26 09:29:47 -0700

    done

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * get_bit - returns the value of a bit at given index
+ * @n: storing the given index values here
+ * @index: the index of the values
+ * Return: return -1 if error accured
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	if (index > sizeof(n) * 8)
+		return (-1);
+	return ((n >> index) & 1);
+}
