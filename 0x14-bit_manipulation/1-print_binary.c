bff8a05847423c3898dde655f94cc434cc7c5001
Mohamedryouk <mohammedabdullah.ism@gmail.com>
2023-10-26 05:42:46 -0700

    done

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_binary- prints the binary representation of number
+ * @n: unsigned int of printed binary
+ */ 
+void print_binary(unsigned long int n)
+{
+	int i;
+	unsigned int bit;
+	for (i = 31; i >= 0; i--)
+	{
+		bit = n >> i;
+		if (bit & 1)
+			printf("1");
+		else
+			printf("0");
+	}
+}
