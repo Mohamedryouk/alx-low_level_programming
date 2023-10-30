de463de5ef3472224f606ac6fbee6114f0c1fbf7
Mohamedryouk <mohammedabdullah.ism@gmail.com>
2023-10-30 14:27:57 -0700

    done

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -8,32 +8,27 @@
 int create_file(const char *filename, char *text_content)
 {
 	int fd;
-	ssize_t write_content;
-	char buffer[1024];
-	int i = 0;
+	int write_content;
+	int read_write;
 
 	if (!filename)
 		return (-1);
-	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
 	/*if the text content is equal to null*/
-	if (text_content || text_content[0] == '\0')
+	if (!text_content)
 	{
 		close(fd);
-		return (-1);
+		text_content = " ";
 	}
-	while (text_content[i] != '\0' && i < 1023)
-	{
-		buffer[i] = text_content[i];
-		i++;
-	}
-	buffer[i] = '\0';
+	
+	for (write_content = 0; text_content[read_write]; read_write++)
+		;
 	/*then we write the content to the file*/
-	write_content = write(fd, buffer, i);
-	if (write_content == -1 || write_content != i)
+	read_write = write(fd, text_content, write_content);
+	if (read_write == -1)
 	{
-		close(fd);
 		return (-1);
 	}
 	close(fd);
