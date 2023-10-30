73ae296f6b6ba52aff9c6c90695eb6fc577fa1df
Mohamedryouk <mohammedabdullah.ism@gmail.com>
2023-10-30 14:36:06 -0700

    done

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,24 +13,22 @@ int create_file(const char *filename, char *text_content)
 
 	if (!filename)
 		return (-1);
+
 	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+
 	if (fd == -1)
 		return (-1);
 	/*if the text content is equal to null*/
 	if (!text_content)
-	{
-		close(fd);
-		text_content = " ";
-	}
+		text_content = "";
 	
-	for (write_content = 0; text_content[read_write]; read_write++)
+	for (write_content = 0; text_content[write_content]; write_content++)
 		;
+	
 	/*then we write the content to the file*/
 	read_write = write(fd, text_content, write_content);
 	if (read_write == -1)
-	{
 		return (-1);
-	}
 	close(fd);
 	return (1);
 }
