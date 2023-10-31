2d4b971d4b415925763819dea3e1a60caee21792
Mohamedryouk <mohammedabdullah.ism@gmail.com>
2023-10-31 03:46:01 -0700

    done

---
+++ 0x15-file_io/100-elf_header.c
@@ -0,0 +1,123 @@
+#include "main.h"
+/**
+ * main- main function
+ * @argc: argunments
+ * @argv: var arguments
+ * Return: return 0
+ */
+int main(int argc, char *argv[])
+{
+	int fd;
+	ElfHeader header;
+
+	if (argc != 2) {
+		fprintf(stderr, "Usage: %s elf_filename\n", argv[0]);
+		return ERROR_INVALID_ELF;
+	}
+
+	fd = open(argv[1], O_RDONLY);
+	if (fd == -1) {
+		perror("Error opening file");
+		return ERROR_INVALID_ELF;
+	}
+
+	if (read(fd, &header, sizeof(ElfHeader)) != sizeof(ElfHeader)) {
+		perror("Error reading ELF header");
+		close(fd);
+		return ERROR_INVALID_ELF;
+	}
+
+	if (header.e_ident[EI_MAG0] != 0x7F || header.e_ident[EI_MAG1] != 'E' ||
+	header.e_ident[EI_MAG2] != 'L' || header.e_ident[EI_MAG3] != 'F') {
+	fprintf(stderr, "Not a valid ELF file\n");
+	close(fd);
+	return ERROR_INVALID_ELF;
+	}
+
+	printf("ELF Header:\n");
+	printElfHeaderInfo(header);
+
+	close(fd);
+
+	return 0;
+}
+/**
+ * printElfHeaderInfo- prints header of elf
+ * @header: constructor
+ */
+void printElfHeaderInfo(ElfHeader header)
+{
+	int i;
+
+	printf("  Magic:   ");
+	for (i = 0; i < 16; i++) {
+		printf("%02x ", header.e_ident[i]);
+	}
+	printf("\n");
+
+	printf("  Class:                             ");
+	switch (header.e_ident[EI_CLASS]) {
+	case ELFCLASS32:
+		printf("ELF32\n");
+		break;
+	case ELFCLASS64:
+		printf("ELF64\n");
+		break;
+	default:
+		printf("Unknown\n");
+	}
+
+	printf("  Data:                              ");
+	switch (header.e_ident[EI_DATA]) {
+	case ELFDATA2LSB:
+		printf("2's complement, little-endian\n");
+		break;
+	case ELFDATA2MSB:
+		printf("2's complement, big-endian\n");
+		break;
+	default:
+		printf("Unknown\n");
+	}
+
+	printf("  Version:                           %d (current)\n", header.e_ident[EI_VERSION]);
+
+	printf("  OS/ABI:                            ");
+	switch (header.e_ident[EI_OSABI]) {
+	case 0:
+		printf("UNIX - System V\n");
+		break;
+	case 1:
+		printf("UNIX - HP-UX\n");
+		break;
+	case 255:
+		printf("Standalone App\n");
+		break;
+	default:
+		printf("Unknown\n");
+	}
+
+	printf("  ABI Version:                       %d\n", header.e_ident[EI_ABIVERSION]);
+
+	printf("  Type:                              ");
+	switch (header.e_type) {
+	case ET_NONE:
+		printf("NONE (None)\n");
+		break;
+	case ET_REL:
+		printf("REL (Relocatable file)\n");
+		break;
+	case ET_EXEC:
+		printf("EXEC (Executable file)\n");
+		break;
+	case ET_DYN:
+		printf("DYN (Shared object file)\n");
+		break;
+	case ET_CORE:
+		printf("CORE (Core file)\n");
+		break;
+	default:
+		printf("Unknown\n");
+	}
+
+	printf("  Entry point address:               0x%08x\n", header.e_entry);
+}
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,17 +1,66 @@
 #ifndef MAIN_H
 #define MAIN_H
 #define BUFFER_SIZE 1024
+#define EI_MAG0 0
+#define EI_MAG1 1
+#define EI_MAG2 2
+#define EI_MAG3 3
+#define EI_CLASS 4
+#define EI_DATA 5
+#define EI_VERSION 6
+#define EI_OSABI 7
+#define EI_ABIVERSION 8
+
+#define ELFCLASS32 1
+#define ELFCLASS64 2
+#define ELFDATA2LSB 1
+#define ELFDATA2MSB 2
+
+#define ET_NONE 0
+#define ET_REL 1
+#define ET_EXEC 2
+#define ET_DYN 3
+#define ET_CORE 4
+
+#define ERROR_INVALID_ELF 98
+
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <stdint.h>
+#include <string.h>
+#include <stdbool.h>
+/**
+ * struct- structers for header
+ * @Elfheader:
+ *
+ */
+typedef struct {
+    unsigned char e_ident[16];
+    uint16_t e_type;
+    uint16_t e_machine;
+    uint32_t e_version;
+    uint32_t e_entry;
+    uint32_t e_phoff;
+    uint32_t e_shoff;
+    uint32_t e_flags;
+    uint16_t e_ehsize;
+    uint16_t e_phentsize;
+    uint16_t e_phnum;
+    uint16_t e_shentsize;
+    uint16_t e_shnum;
+    uint16_t e_shstrndx;
+} ElfHeader;
+
 ssize_t read_textfile(const char *filename, size_t letters);
 int _putchar(char c);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
-int copy_function(int argc, char *argv[]);
-
+int main(int argc, char *argv[]);
+void error_file(int file_from, int file_to, char *argv[]);
+void printElfHeaderInfo(ElfHeader header);
 
 #endif /*MAIN.H*/
