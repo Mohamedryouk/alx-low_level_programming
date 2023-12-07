f421be85672f7cbc7ff8baf6a230bd1bacebba37
root <root@DESKTOP-S8OJ033.>
2023-12-07 16:42:54 +0200

    done

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -6,13 +6,14 @@
  **/
 size_t print_dlistint(const dlistint_t *h)
 {
-    const dlistint_t *elements = h;
-    size_t count = 0;
-    while(elements != NULL)
-    {
-        printf("%d\n", elements->n);
-        elements = elements->next;
-        count++;
-    }
-    return count;
+        const dlistint_t *elements = h;
+        size_t count = 0;
+
+        while(elements != NULL)
+        {
+                printf("%d\n", elements->n);
+                elements = elements->next;
+                count++;
+        }
+        return (count);
 }
