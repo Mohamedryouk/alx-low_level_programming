515a3f3bcb7750a9f4aa5f3ba5ee2625b2467dac
Mohamedryouk <mohammedabdullah.ism@gmail.com>
2023-12-07 06:50:01 -0800

    done

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,19 +1,19 @@
 #include "lists.h"
 /**
-**print_dlinstint - return list element
-**@h: pointer of header
-**Return: returns the elements
-**/
+ *print_dlinstint - return list element
+ *@h: pointer of header
+ *Return: returns the elements
+ **/
 size_t print_dlistint(const dlistint_t *h)
 {
-        const dlistint_t *elements = h;
-        size_t count = 0;
+	const dlistint_t *elements = h;
+	size_t count = 0;
 
-        while(elements != NULL)
-        {
-                printf("%d\n", elements->n);
-                elements = elements->next;
-                count++;
-        }
-        return (count);
+	while (elements != NULL)
+	{
+		printf("%d\n", elements->n);
+		elements = elements->next;
+		count++;
+	}
+	return (count);
 }
