6db98d40ac318fb1ed7dcc55ee409cbe5e2dbedb
root <root@DESKTOP-S8OJ033.>
2023-12-07 16:34:05 +0200

    done

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,4 +1,9 @@
 #include "lists.h"
+/**
+ *print_dlinstint - return list element
+ *@h: pointer of header
+ *Return: returns the elements
+ **/
 size_t print_dlistint(const dlistint_t *h)
 {
     const dlistint_t *elements = h;
