004ae805f143a9e71915136c09929779489ef791
root <root@DESKTOP-S8OJ033.>
2023-12-08 16:04:37 +0200

    done

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	dlistint_t *current = head;
+	unsigned int i = 0;
+
+    while (current != NULL && i < index)
+    {
+        current = current->next;
+        i++;
+    }
+    if (i == index && current != NULL)
+    {
+        return current;
+    }
+    else
+    {
+        return (NULL);
+    }
+}
