ab37b8374d47de297a487d6368af42c8d8e661f5
root <root@DESKTOP-S8OJ033.>
2023-12-08 21:38:09 +0200

    done

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -8,20 +8,46 @@
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
+	unsigned int i;
 	dlistint_t *new_node = malloc(sizeof(dlistint_t));
+	new_node = *h;
 
+	if (h == NULL)
+	{
+		return (NULL);
+	}
 	if (new_node == NULL)
 	{
 		return (NULL);
 	}
 	new_node->n = n;
-	new_node->prev = next->prev;
-	next->prev = new_node;
-	new_node->next = new_node;
-	if (new_node->prev != NULL)
+	new_node->prev = NULL;
+	new_node->next = NULL;
+	if (idx == 0)
 	{
-		new_node->prev->next = new_node;
+		new_node->next = *h;
+		if (*h != NULL)
+		{
+			(*h)->prev = new_node;
+		}
 		*h = new_node;
+		return (new_node);
 	}
+	for (i = 0; i < idx - 1 && new_node != NULL; i++)
+	{
+		new_node = new_node->next;
+	}
+	if (new_node == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+	new_node->prev = new_node;
+	new_node->next = new_node->next;
+	if (new_node->next != NULL)
+	{
+		new_node->next->prev = new_node;
+	}
+	new_node->next = new_node;
 	return (new_node);
 }
