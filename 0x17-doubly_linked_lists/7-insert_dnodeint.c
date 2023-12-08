ecafab6639545898822806ee56c00e8a9dbdd5ae
root <root@DESKTOP-S8OJ033.>
2023-12-08 21:11:05 +0200

    done

---
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *new_node = malloc(sizeof(dlistint_t));
+
+	if (new_node == NULL)
+	{
+		return (NULL);
+	}
+	new_node->n = n;
+	new_node->prev = next->prev;
+	next->prev = new_node;
+	new_node->next = new_node;
+	if (new_node->prev != NULL)
+	{
+		new_node->prev->next= new_node;
+		*h = new_node;
+	}
+	return (new_node);
+}
