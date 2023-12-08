76ef7742b25e64a549268c540b5be73cb252afda
root <root@DESKTOP-S8OJ033.>
2023-12-08 22:36:28 +0200

    done

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -1,4 +1,10 @@
 #include "lists.h"
+/**
+ *delete_dnodeint_at_index - lists delete
+ *@head:
+ *@idx: indexs
+ *Return: return lists
+ */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index) {
 	unsigned int i;
 	dlistint_t *current = *head;
