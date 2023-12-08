53a6ffc26315ee7e9fb4d1b2fc605dd333ff3aca
root <root@DESKTOP-S8OJ033.>
2023-12-08 10:55:05 +0200

    done

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -14,11 +14,12 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
         {
                 return (NULL);
         }
-        new_node->n = n;
         last = *head;
+        new_node->n = n;
         new_node->next = NULL;
         if (*head == NULL)
         {
+                new_node->prev = NULL;
                 *head = new_node;
                 return (NULL);
         }
@@ -27,5 +28,6 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
                 last = last->next;
         }
         last->next = new_node;
+        new_node->prev = last;
         return (new_node);
 }
