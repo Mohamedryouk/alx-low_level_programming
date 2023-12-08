5f6fbf53b125d643ad28ed6286398e6db55df168
root <root@DESKTOP-S8OJ033.>
2023-12-08 16:31:30 +0200

    done

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -1,7 +1,13 @@
 #include "lists.h"
+/**
+ *sum_dlistint - sum of list
+ *@head: head of list
+ *Return: return the sum of the list
+ */
 int sum_dlistint(dlistint_t *head)
 {
         int sum = 0;
+
         if (!head)
         {
                 return (0);
