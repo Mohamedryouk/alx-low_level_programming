624810e85c4942e8dcfee6331c7e313313e7b2fc
root <root@DESKTOP-S8OJ033.>
2023-12-08 09:47:27 +0200

    done

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -2,8 +2,7 @@
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
         dlistint_t *new_node = malloc(sizeof(dlistint_t));
-
-        new_node->prev = NULL;
+ 
         new_node->n = n;
         new_node->next = NULL;
         new_node->next = (*head);
