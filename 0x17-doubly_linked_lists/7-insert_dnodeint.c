d1cfc77032385e59689843797044e07054596274
root <root@DESKTOP-S8OJ033.>
2023-12-08 22:31:44 +0200

    done

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -12,9 +12,9 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *new_node = malloc(sizeof(dlistint_t));
 	dlistint_t *current = *h;
 	if (h == NULL)
-        return NULL;
+        return (NULL);
     if (new_node == NULL)
-        return NULL;
+        return (NULL);
 
     new_node->n = n;
     new_node->prev = NULL;
@@ -33,7 +33,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
     if (current == NULL) {
         free(new_node);
-        return NULL;
+        return (NULL);
     }
     new_node->prev = current;
     new_node->next = current->next;
@@ -43,5 +43,5 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
     current->next = new_node;
 
-    return new_node;
+    return (new_node);
 }
