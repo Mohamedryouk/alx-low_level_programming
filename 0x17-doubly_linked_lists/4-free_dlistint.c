20ea95d7e698f8825af1ea91f46114c9dea7934f
Mohamedryouk <mohammedabdullah.ism@gmail.com>
2023-12-08 05:47:05 -0800

    done

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -7,12 +7,12 @@
 void free_dlistint(dlistint_t *head)
 {
 	dlistint_t *current = head;
-    dlistint_t *new_node;
+	dlistint_t *new_node;
 
 	while (current != NULL)
 	{
 		new_node = current->next;
-        free(current);
+		free(current);
 		current = new_node;
 	}
 }
