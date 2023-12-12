65741286dda1adc2c36d54936c9b8ca9029a4d66
root <root@DESKTOP-S8OJ033.>
2023-12-12 09:53:10 +0200

    done

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+/**
+ *add - add two number
+ *@a: first
+ *@b: second
+ *Return: return
+ */
+int add(int a, int b) {
+    return a + b;
+}
+/**
+ *sub - add two number
+ *@a: first
+ *@b: second
+ *Return: return
+ */
+int sub(int a, int b) {
+    return a - b;
+}
+/**
+ *mul - add two number
+ *@a: first
+ *@b: second
+ *Return: return
+ */
+int mul(int a, int b) {
+    return a * b;
+}
+/**
+ *div - add two number
+ *@a: first
+ *@b: second
+ *Return: return
+ */
+int div(int a, int b) {
+    if (b == 0) {
+        fprintf(stderr, "Error: Division by zero\n");
+        return 0;
+    }
+    return a / b;
+}
+/**
+ *mod - add two number
+ *@a: first
+ *@b: second
+ *Return: return
+ */
+int mod(int a, int b) {
+    if (b == 0) {
+        fprintf(stderr, "Error: Modulus by zero\n");
+        return 0;
+    }
+    return a % b;
+}
