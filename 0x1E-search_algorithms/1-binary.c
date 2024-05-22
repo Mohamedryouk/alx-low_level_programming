61bc20e9a7c88c9ff7b67b92f9c886942d509c7e
Mohamedryouk <mohammedabdullah.ism@gmail.com>
2024-05-22 09:31:01 +0300

    done

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -3,22 +3,26 @@
 /**
  * print_subarray - print array after every change.
  * @array: array to search in.
- * @size: size of the array.
- * @value: the value the value that needs to be found.
+ * @x: size of the array.
+ * @y: the value the value that needs to be found.
 */
-void print_subarray(int *array, size_t x, size_t y) {
-    size_t i;
-    printf("Searching in array: ");
-    for (i = x; i <= y; i++) {
-        printf("%d", array[i]);
-        if (i < y) {
-            printf(", ");
-        }
-    }
-    printf("\n");
+void print_subarray(int *array, size_t x, size_t y)
+{
+	size_t i;
+
+	printf("Searching in array: ");
+	for (i = x; i <= y; i++)
+	{
+		printf("%d", array[i]);
+		if (i < y)
+		{
+			printf(", ");
+		}
+	}
+	printf("\n");
 }
 /**
- * print_subarray - print array after every change.
+ * binary_search - print array after every change.
  * @array: array to search in.
  * @size: size of the array.
  * @value: the value the value that needs to be found.
@@ -26,22 +30,22 @@ void print_subarray(int *array, size_t x, size_t y) {
 */
 int binary_search(int *array, size_t size, int value)
 {
-    size_t mid;
-    size_t x = 0, y = size - 1;
+	size_t mid;
+	size_t x = 0, y = size - 1;
 
-    if (array == NULL)
-        return (-1);
+	if (array == NULL)
+		return (-1);
 
-    while (x <= y)
-    {
-        print_subarray(array, x, y);
-        mid = (x + y) / 2;
-        if (value == array[mid])
-            return (mid);
-        else if (value < array[mid])
-            y = mid - 1;
-        else
-            x = mid + 1;
-    }
-    return (-1);
+	while (x <= y)
+	{
+		print_subarray(array, x, y);
+		mid = (x + y) / 2;
+		if (value == array[mid])
+			return (mid);
+		else if (value < array[mid])
+			y = mid - 1;
+		else
+			x = mid + 1;
+	}
+	return (-1);
 }
